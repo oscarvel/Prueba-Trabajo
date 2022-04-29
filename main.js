1fae6534d3eba5d844b64f8200d1a223aff42586
oscarvel <oscarvel96@gmail.com>
2022-04-28 23:44:54 -0500

    Prueba final envio

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(".container");
+ScrollReveal().reveal(".anuncio", { delay: 500 });
+ScrollReveal().reveal(".parrafoIndicativo", { delay: 500 });
+ScrollReveal().reveal(".rectangulo", { delay: 500 });
